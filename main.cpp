ea713510b515e5d57dc2b8eb2a62fb2485a20a09
Alex Webb <alexwebbb@protonmail.com>
2017-09-23 12:19:47 -0500

    my own function

--- main.cpp
+++ main.cpp
@@ -9,26 +9,29 @@ using namespace this_thread; // sleep_for, sleep_until
 using namespace chrono; // nanoseconds, system_clock, seconds
 
 
+void say(string phrase, int delay = 1500) {
+	sleep_for(milliseconds(delay));
+	cout << phrase << endl;
+}
+
 int main(int argc, char **argv) {
 
 	constexpr int WORD_LENGTH = 5;
 	string Guess = "";
 	
-	sleep_for(milliseconds(8000));
-	cout << "Lets play some bulls and COWS yall!" << endl;
+	say("Lets play some bulls and COWS yall!", 2000);
+	say("I got a secret word, yall all gotta try and guess it.");
 	sleep_for(milliseconds(1500));
-	cout << "I got a secret word, yall all gotta try and guess it." << endl;
-	sleep_for(milliseconds(1500));
-	cout << "Heh, I'll make it easy for ya, its " << WORD_LENGTH << " long," << endl;
-	sleep_for(milliseconds(1500));
-	cout << "an' I ain' use any letter more than once." << endl;
+	cout << "Heh, I'll make it easy for ya, its " << WORD_LENGTH << " long, " << endl;
+	say("an' I ain' use any letter more than once.");
 	for (size_t i = 0; i < 3; i++)
 	{
 		sleep_for(milliseconds(1500));
 		cout << ".";
 	}
 	cout << "Gimme that guess! (Hint: Enter your guess) ";
-	cin >> Guess;
+	getline(cin, Guess);
+	Guess.resize(WORD_LENGTH);
 	cout << "Your guess is " << Guess << "?? Wild" << endl;
 
 	return 0;
