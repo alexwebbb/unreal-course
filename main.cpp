609280f0d79050b2f8b8eedf9e124e8e822afdf3
Alex Webb <alexwebbb@protonmail.com>
2017-09-23 12:53:39 -0500

    refactor using signature pattern

--- main.cpp
+++ main.cpp
@@ -9,31 +9,51 @@ using namespace this_thread; // sleep_for, sleep_until
 using namespace chrono; // nanoseconds, system_clock, seconds
 
 
-void say(string phrase, int delay = 1500) {
-	sleep_for(milliseconds(delay));
-	cout << phrase << endl;
-}
+void Say(string phrase, int delay);
+void PrintIntro();
+string GetGuess();
 
-int main(int argc, char **argv) {
-
-	constexpr int WORD_LENGTH = 5;
+int main(int argc, char **argv) 
+{
 	string Guess = "";
 	
-	say("Lets play some bulls and COWS yall!", 2000);
-	say("I got a secret word, yall all gotta try and guess it.");
-	sleep_for(milliseconds(1500));
-	cout << "Heh, I'll make it easy for ya, its " << WORD_LENGTH << " long, " << endl;
-	say("an' I ain' use any letter more than once.");
-	for (size_t i = 0; i < 3; i++)
-	{
-		sleep_for(milliseconds(1500));
-		cout << ".";
-	}
-	cout << "Gimme that guess! (Hint: Enter your guess) ";
-	getline(cin, Guess);
-	Guess.resize(WORD_LENGTH);
+	PrintIntro();
+	Guess = GetGuess();
 	cout << "Your guess is " << Guess << "?? Wild" << endl;
 
 	return 0;
 
+}
+
+void Say(string phrase, int delay = 1500)
+{
+	sleep_for(milliseconds(delay));
+	cout << phrase << endl;
+}
+
+void PrintIntro()
+{
+	constexpr int WORD_LENGTH = 9;
+
+	Say("Lets play some bulls and COWS yall!", 2000);
+	Say("I got a secret word, yall all gotta try and guess it.");
+	sleep_for(milliseconds(1500));
+	cout << "Heh, I'll make it easy for ya, its " << WORD_LENGTH << " long,\n";
+	Say("an' I ain' use any letter more than once.");
+
+	return;
+}
+
+string GetGuess()
+{
+	string Guess = "";
+
+	for (size_t i = 0; i < 5; i++)
+	{
+		sleep_for(milliseconds(400));
+		cout << ".";
+	}
+	cout << "Gimme that guess! \n(Hint: Enter your guess) ";
+	getline(cin, Guess);
+	return Guess;
 }
